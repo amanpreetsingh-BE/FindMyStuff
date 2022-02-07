b7a717a6d436f540fdc9b23af8d909a04ac2d72a
Amanpreet singh <amanpreet@outlook.be>
2022-02-07 21:51:05 +0100

    i18

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,9 @@
+import path from 'path'
+
 module.exports = {
     i18n: {
       defaultLocale: 'en',
       locales: ['en', 'fr'],
+      localePath: path.resolve('./public/static/locales')
     },
 };
