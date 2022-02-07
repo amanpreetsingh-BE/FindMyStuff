d6592e44b7ec7d29341dbe17490494d12c840856
Amanpreet singh <amanpreet@outlook.be>
2022-02-07 21:55:16 +0100

    fix i18n config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,9 @@
-import path from 'path'
+const path = require('path');
 
 module.exports = {
     i18n: {
       defaultLocale: 'en',
       locales: ['en', 'fr'],
-      localePath: path.resolve('./public/static/locales')
+      localePath: path.resolve('./public/locales')
     },
 };
