3853afd8fd1b97d14a4df86cbb301c5df0098716
Amanpreet singh <amanpreet@outlook.be>
2022-03-17 11:42:54 +0100

    Solved CORS policy for message section

--- next.config.js
+++ next.config.js
@@ -4,16 +4,5 @@ module.exports = {
   i18n,
   images: {
     domains: ['firebasestorage.googleapis.com', 'ww2.mondialrelay.com'],
-  },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "https://www.findmystuff.io" },
-        ]
-      }
-    ]
-  },
+  }
 }
