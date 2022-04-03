4bd012617d9396928c0648a87db3ff918ce55c81
Amanpreet singh <amanpreet@outlook.be>
2022-04-03 16:14:38 +0200

    corrected missing firebase auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,18 @@
-import '@styles/globals.css'
-import { appWithTranslation } from 'next-i18next';
-import { Toaster } from 'react-hot-toast'
-import { UserContext } from '@lib/context'
-import { useUserData } from '@lib/hooks'
+import "@styles/globals.css";
+import { appWithTranslation } from "next-i18next";
+import { Toaster } from "react-hot-toast";
+import { UserContext } from "@lib/context";
+import { useUserData } from "@lib/hooks";
 
 function MyApp({ Component, pageProps }) {
-
-  const userData = useUserData()
+  const userData = useUserData();
 
   return (
     <UserContext.Provider value={userData}>
       <Component {...pageProps} />
       <Toaster />
     </UserContext.Provider>
-  )
+  );
 }
 
-
-export default appWithTranslation(MyApp)
+export default appWithTranslation(MyApp);
